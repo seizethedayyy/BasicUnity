41d8c3124f360fe86aea15f4c7839e7382a91abc
seizethedayyy <kjm4803@naver.com>
2025-03-10 11:25:27 +0900

    study

---
+++ BasicStudy/Assets/Script/VariableExample.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+
+public class VariableExample : MonoBehaviour
+{
+
+    //변수 선언
+    public int playerScore = 0;
+    public float speed = 5.5f;
+    public string playerName = "Hero";
+    public bool isGameOver = false;
+
+    void Start()
+    {
+        //변수출력
+        Debug.Log("Player Name: " + playerName);
+        Debug.Log("Player Score: " + playerScore);
+        Debug.Log("Speed: " + speed);
+        Debug.Log("Is Game Over: " + isGameOver);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
